610ba1b6623a6619060c909d4fc5f5ac40fb1be2
Bei Dai <bei.dai@kellogg.northwestern.edu>
2023-04-21 19:54:52 +0000

    1

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -1,10 +1,20 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 3-arrays.rb
+favorite_foods = ["tacos", "burritos", "nachos"]
+puts favorite_foods.inspect
+
+numbers = [4, 8, 15, 16, 23, 42]
+puts numbers.inspect
+
+puts numbers
 
 # Arrays are lists of things
+shopping_lists = [["milk", "eggs", "toilet paper"], ["soap", "shampoo"]]
+puts shopping_lists.inspect
 
 # Accessing the array
+puts favorite_foods[0] #zero is first element of array
 
 # Add to the array
 
